1e393ee2457f4397e6ff03059b3bd5f395c79c53
enumura1 <akirayuto0718@gmail.com>
2024-01-08 15:01:45 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <section>
+        <div>hoge1</div>
+      </section>
+      <section>
+        <div>hoge2</div>
+      </section>
+      <section>
+        <div>hoge3</div>
+      </section>
+    </>
+  )
+}
